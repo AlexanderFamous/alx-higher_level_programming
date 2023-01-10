2c9b9a57707a56bd574158b3f49b67ce30690a01
Donchocho1 <ogagaoghene11@gmail.com>
2023-01-10 11:33:24 -0800

    First Commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     i = 0
     if len(argv[:-1]) == 1:
-        print(len(argv[:-1]), "argument:", end="\n")
-    elif len(argv[:-1]) == 2:
-        print(len(argv[:-1]), "arguments:", end="\n")
+        print(len(argv[:-1]), "argument:")
+    elif len(argv[:-1]) >= 2:
+        print(len(argv[:-1]), "arguments:")
     else:
         print(len(argv[:-1]), "arguments.")
     for i, arg in enumerate(argv[1:]):
