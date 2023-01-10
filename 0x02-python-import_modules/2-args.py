6f61188f77b4aca0fb030c0ece1aac9af701fe73
Donchocho1 <ogagaoghene11@gmail.com>
2023-01-10 11:12:49 -0800

    First Commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = 0
+    if len(sys.argv[:-1]) >= 1:
+        print(len(sys.argv[:-1]),"arguments:",end="\n")
+        for i, arg in enumerate(sys.argv[1:]):
+            i = i + 1
+            print("{}:  {}".format(i,arg))
+    else:
+        print(len(sys.argv[:-1]),"argument.")
