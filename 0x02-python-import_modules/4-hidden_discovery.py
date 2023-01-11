1528529769f1ff620c954d5becfab3def841fddd
Donchocho1 <ogagaoghene11@gmail.com>
2023-01-10 16:48:09 -0800

    First Commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    
+    import hidden_4
+    names = dir(hidden_4)
+    for i in names:
+        if i[0] == "_" and i[1] == "_":
+            continue
+        print(i)
