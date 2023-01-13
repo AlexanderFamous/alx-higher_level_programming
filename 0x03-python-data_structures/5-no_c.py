11bbec9ddb124970da0cdea98f80c610a58e23ec
Donchocho1 <ogagaoghene11@gmail.com>
2023-01-12 19:32:50 -0800

    First Commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     new_str = ""
     for i in (my_string):
-        if i == 'c' or  i == 'C':
+        if i == 'c' or i == 'C':
             pass
         else:
-            new_str += i 
+            new_str += i
     return new_str
